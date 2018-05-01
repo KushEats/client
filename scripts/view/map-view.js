1fa8b35eb014825ea7de94b6ca308b0ac32ef90c
khalahm253 <khalahm253@gmail.com>
2018-05-01 11:01:36 -0700

    test

---
+++ scripts/view/map-view.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var map = {};
+var locations = [];
+
+function initMap () {
+      //Some of the code syntax used was given by the google maps API website.
+      map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 47.608013, lng: -122.335167 },
+      zoom: 12,
+      mapTypeId: 'roadmap'
+    });
+
+    
+}
