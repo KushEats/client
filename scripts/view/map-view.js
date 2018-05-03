99d6aad61e971151427b9e134a5f8a926307be89
Stephen <stephendeanharper@gmail.com>
2018-05-03 09:39:01 -0700

    updated map-view

--- scripts/view/map-view.js
+++ scripts/view/map-view.js
@@ -92,21 +92,3 @@ function initAutocomplete() {
     map.fitBounds(bounds);
   });
 }
-
-
-
-
-
-
-
-
-
-
-// function initMap() {
-//   //Some of the code syntax used was given by the google maps API website.
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: { lat: 47.608013, lng: -122.335167 },
-//     zoom: 12,
-//     mapTypeId: 'roadmap'
-//   });
-// }
