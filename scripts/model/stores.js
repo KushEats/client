3d8339a5c98cb9a9763095a728d9bec750b39d73
Stephen <stephendeanharper@gmail.com>
2018-05-03 14:32:42 -0700

    added in the heroku site productionapiurl

--- scripts/model/stores.js
+++ scripts/model/stores.js
@@ -5,7 +5,7 @@ var app = app || {};
 const ENV = {};
 
 ENV.isProduction = window.location.protocol === 'https:';
-ENV.productionApiUrl = 'insert heroku site';
+ENV.productionApiUrl = 'https://kush-eats.herokuapp.com/';
 ENV.developmentApiUrl = 'http://localhost:3000';
 
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
